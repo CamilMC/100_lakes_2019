82cfdee0c614028547005102de8c4a18f38a61e1
Camille Crapart <c.m.crapart@kjemi.uio.no>
2020-12-09 08:19:34 +0100

    Removes charge balance

--- 8.version_control/id_key.R
+++ 8.version_control/id_key.R
@@ -1,10 +1,11 @@
-id_niva <- readxl::read_excel("7.1000_lakes/id_niva.xlsx")
-id_cba <- readxl::read_excel("5.100_lakes/100lakesData.xlsx") %>% distinct(lake_id,.keep_all = T)
-nve_number <- readxl::read_excel("5.100_lakes/100lakes_gases.xlsx") %>% select(c("Lake_ID","NVE_number")) %>% 
-  distinct(Lake_ID,.keep_all = T)
+library(dplyr)
 
-id_key <- merge(id_cba,id_niva,by.x="lake_id",by.y="Lake_ID") %>% 
-  merge(nve_number,by.x = "lake_id", by.y = "Lake_ID")
+id_niva <- readxl::read_excel("7.1000_lakes/id_niva.xlsx")
+id_cba <- readxl::read_excel("5.100_lakes/100Lakes_info.xlsx")
+#nve_number <- readxl::read_excel("5.100_lakes/100lakes_gases.xlsx") %>% select(c("Lake_ID","NVE_number")) %>% 
+#  distinct(Lake_ID,.keep_all = T)
+
+id_key <- merge(id_cba,id_niva,by.x="lake_id",by.y="Lake_ID")
 id_key$temp <- NULL
 id_key$cond. <- NULL
 id_key$pH_sampling <- NULL
